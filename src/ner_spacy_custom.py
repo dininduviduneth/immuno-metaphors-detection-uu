723922d18379ada95f7dfad01024ef95e5f04ed6
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2023-11-21 14:55:48 +0100

    update train script
    Also minor changes to handling of failing to create certain doc spans

--- src/ner_spacy_custom.py
+++ src/ner_spacy_custom.py
@@ -5,9 +5,12 @@ from sklearn.model_selection import train_test_split
 
 
 def main():
-    annotations = json.load(open("../data/immun_meta_anno.json", "r"))
+    annotations = json.load(open("../data/spacy_annotations.json", "r"))
     annotations = annotations["annotations"]
-    train, test = train_test_split(annotations, test_size=0.1)
+    train, test = train_test_split(annotations, test_size=0.33)
+    train, dev = train_test_split(train, test_size=0.2)
+    print(len(train),len(dev))
+    # print(train, test)
     nlp = spacy.blank("en")
     db = DocBin()
     for text, data in train:
@@ -21,24 +24,64 @@ def main():
                 if idx in entity_indices:
                     skip_entity = True
                     break
+            if start == end:
+                skip_entity = True 
             if skip_entity:
                 continue
 
             entity_indices = entity_indices + list(range(start, end))
             try:
+                # print("creating span, or trying to")
+                # print(text[start:end])
                 span = doc.char_span(start, end, label=label, alignment_mode="strict")
             except:
                 continue
 
             if span is None:
                 # Log errors for annotations that couldn't be processed
-                err_data = str([start, end]) + "    " + str(text) + "\n"
+                err_data = f"[{start, end}, ({text[start:end]})]\t{text}\n"
                 print(err_data)
             else:
                 ents.append(span)
         doc.ents = ents
         db.add(doc)
     db.to_disk("../spacy/train.spacy")
+    nlp = spacy.blank("en")
+    db = DocBin()
+    for text, data in dev:
+        entity_indices = []
+        ents = []
+        doc = nlp.make_doc(text)
+        annot = data["entities"]
+        for start, end, label in annot:
+            skip_entity = False
+            for idx in range(start, end):
+                if idx in entity_indices:
+                    skip_entity = True
+                    break
+            if start == end:
+                skip_entity = True 
+            
+            if skip_entity:
+                continue
+
+            entity_indices = entity_indices + list(range(start, end))
+            try:
+                # print("creating span, or trying to")
+                # print(text[start:end])
+                span = doc.char_span(start, end, label=label, alignment_mode="strict")
+            except:
+                continue
+
+            if span is None:
+                # Log errors for annotations that couldn't be processed
+                err_data = f"[{start, end}, ({text[start:end]})]\t{text}\n"
+                print(err_data)
+            else:
+                ents.append(span)
+        doc.ents = ents
+        db.add(doc)
+    db.to_disk("../spacy/test.spacy")
     nlp = spacy.blank("en")
     db = DocBin()
     for text, data in test:
@@ -52,6 +95,8 @@ def main():
                 if idx in entity_indices:
                     skip_entity = True
                     break
+            if start == end:
+                skip_entity = True 
             if skip_entity:
                 continue
 
@@ -62,14 +107,16 @@ def main():
                 continue
 
             if span is None:
+                # print("creating span, or trying to")
+                # print(text[start:end])
                 # Log errors for annotations that couldn't be processed
-                err_data = str([start, end]) + "    " + str(text) + "\n"
+                err_data = f"[{start, end}, ({text[start:end]})]\t{text}\n"
                 print(err_data)
             else:
                 ents.append(span)
         doc.ents = ents
         db.add(doc)
-    db.to_disk("../spacy/test.spacy")
+    db.to_disk("../spacy/validation.spacy")
 
 
 if __name__ == "__main__":
--- src/spacy_train.sh
+++ src/spacy_train.sh
@@ -1,1 +1,2 @@
+rm -rfv ../data/output
 python3 -m spacy train ../spacy/config.cfg --output ../spacy/output --paths.train ../spacy/train.spacy --paths.dev ../spacy/test.spacy
