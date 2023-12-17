5350a23999ab13e1118b0b07d9826205923babbf
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2023-12-17 11:23:31 +0100

    skip sentence in case of too many tokens

--- src/production/2_sentence_generator.py
+++ src/production/2_sentence_generator.py
@@ -12,14 +12,14 @@ for paper in data:
     abstract = paper.get("abstract")
     abs_sentences = split_sentences(abstract)  # sent_tokenize(abstract)
     for sentence in abs_sentences:
-        temp = {"sentence": sentence, "metaphors": [], "has_metaphor": 0}
-        for metaphor in metaphors:
-            if metaphor.lower() in sentence.lower():
-                temp["has_metaphor"] = 1
-                temp["metaphors"].append(metaphor)
+        if len(sentence) < 512:
+            temp = {"sentence": sentence, "metaphors": [], "has_metaphor": 0}
+            for metaphor in metaphors:
+                if metaphor.lower() in sentence.lower():
+                    temp["has_metaphor"] = 1
+                    temp["metaphors"].append(metaphor)
 
-        sentences.append(temp)
-        ##print("Appended: " + str(paper.get("paperId")))
+            sentences.append(temp)
 
 print("Extracted " + str(len(sentences)) + " sentences!")
 
